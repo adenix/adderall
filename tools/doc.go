eb094ffabf45602bb8c591c9fa3ffb5777e06642
Austin Nicholas <austin@adenix.io>
2022-01-11 00:02:01 -0600

    Added doc for tools package

---
+++ tools/doc.go
@@ -0,0 +1,4 @@
+// Package tools exists to make this directory a valid Go package.
+// The tools.go has a build tag that excludes it from being considered by Go
+// tooling except for dependency constraints.
+package tools
